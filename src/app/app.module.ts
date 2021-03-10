cae3b34a7168ff1015b5cab09a80fbb20aee94b7
bluewolfx <ricardobanon@gmail.com>
2021-03-10 01:47:06 +0000

    Created products-alerts folders with files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductsAlertsComponent } from './products-alerts/products-alerts.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import { ProductsAlertsComponent } from './products-alerts/products-alerts.compo
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductsAlertsComponent
+    ProductsAlertsComponent,
+    ProductAlertsComponent
   ],
   bootstrap: [
     AppComponent
